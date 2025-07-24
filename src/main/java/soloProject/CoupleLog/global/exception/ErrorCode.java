747fd25df0402bdec5de274c621c0d91d58cb380
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-07-24 23:25:52 +0900

    feat: 기본 서버 에러 코드 추가

--- src/main/java/soloProject/CoupleLog/global/exception/ErrorCode.java
+++ src/main/java/soloProject/CoupleLog/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "1", "카테고리를 찾을 수 없습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "1", "서버내부 에러가 발생했습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "2", "카테고리를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
